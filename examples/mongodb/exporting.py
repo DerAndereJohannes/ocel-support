f926ea53cc212a6d256295bf7cac77757a9cd773
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-07-29 15:17:47 +0200

    added timing to importing/exporting
    added timing to importing/exporting

--- examples/mongodb/exporting.py
+++ examples/mongodb/exporting.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 import ocel
 from ocel.exporter import mongodb as mongodb_exporter
 from examples.mongodb.commons import CONNECTION_STRING, DATABASE_NAME
+import time
 
 
 def execute_script():
@@ -14,7 +15,10 @@ def execute_script():
     db = client[DATABASE_NAME]
     log = ocel.import_log("../../logs/log.xmlocel")
 
+    aa = time.time_ns()
     mongodb_exporter.apply(log, db)
+    bb = time.time_ns()
+    print("\nTOTAL TIME: ",(bb-aa)/10**9)
 
 
 if __name__ == "__main__":
--- examples/mongodb/importing.py
+++ examples/mongodb/importing.py
@@ -2,13 +2,18 @@ from pymongo import MongoClient
 import ocel
 from ocel.importer import mongodb as mongodb_importer
 from examples.mongodb.commons import CONNECTION_STRING, DATABASE_NAME
+import time
 
 
 def execute_script():
     client = MongoClient(CONNECTION_STRING)
     db = client[DATABASE_NAME]
     log = mongodb_importer.apply(db)
+
+    aa = time.time_ns()
     ocel.export_log(log, "prova.jsonocel")
+    bb = time.time_ns()
+    print("\nTOTAL TIME: ",(bb-aa)/10**9)
 
 
 if __name__ == "__main__":
