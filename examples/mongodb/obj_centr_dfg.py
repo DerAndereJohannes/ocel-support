7ade422ea2850c0308b98fa8ca3a8484ee23799e
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2021-07-29 14:32:13 +0200

    intermediate commit
    intermediate commit

---
+++ examples/mongodb/obj_centr_dfg.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+from examples.mongodb.commons import CONNECTION_STRING, DATABASE_NAME
+import time
+from collections import Counter
+
+def execute_script():
+    client = MongoClient(CONNECTION_STRING)
+    db = client[DATABASE_NAME]
+    events_collection = db["ocel:events"]
+    objects_collection = db["ocel:objects"]
+    mdfg = {}
+    aa = time.time_ns()
+    object_types = objects_collection.distinct("ocel:type")
+    ggg = events_collection.aggregate(
+        [{"$unwind": "$ocel:omap"}, {"$group": {'_id': '$ocel:omap', 'Subject': {"$push": '$ocel:activity'}}}])
+    ggg = {x["_id"]: x["Subject"] for x in ggg}
+    for ot in object_types:
+        objects = objects_collection.find({"ocel:type": ot}).distinct("ocel:id")
+        for gk in ggg:
+            print(gk)
+
+    bb = time.time_ns()
+    print("\nTOTAL TIME: ",(bb-aa)/10**9)
+    print(mdfg)
+
+
+if __name__ == "__main__":
+    execute_script()
